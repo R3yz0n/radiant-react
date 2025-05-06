8db2cd827e42436c907dc58f6e55b54bbb789cfc
r3yz0n <reyzon.thapa2020@gmail.com>
2025-05-06 14:28:58 +0545

    create:navbar component

--- understanding-components/src/App.jsx
+++ understanding-components/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Navbar from "./components/Navbar";
 const App = () => {
-  return <div className="wrapper">App</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
